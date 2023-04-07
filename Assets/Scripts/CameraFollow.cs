dac5c8a61793b0debce3a44a386ccd8714f4e721
Yurkin-phr <Artem6isk@gmail.com>
2023-04-07 13:42:54 +0700

    7.04 finish

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
+    [SerializeField] private Transform target = null;
+
+    private Vector3 offset;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        offset = transform.position - target.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = Vector3.Lerp(transform.position, new Vector3(target.position.x, 2, target.position.z) + offset, Time.deltaTime * 3);
     }
 }
